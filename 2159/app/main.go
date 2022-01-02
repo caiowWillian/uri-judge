b8c7c90900948521b8eba41730daf990dae5efb2
Caio Willian <caiow.willian@gmail.com>
2022-01-02 00:42:34 -0300

    chore: change return mode

--- 2159/app/main.go
+++ 2159/app/main.go
@@ -16,8 +16,9 @@ func main() {
 	fmt.Printf("%.1f %.1f\n", minimum, maximum)
 }
 
-func Schoenfeld(n int) (float64, float64) {
+func Schoenfeld(n int) (minimum float64, maximum float64) {
 	const a = 1.25506
-	minimum := float64(n) / math.Log(float64(n))
-	return minimum, a * minimum
+	minimum = float64(n) / math.Log(float64(n))
+	maximum = a * minimum
+	return
 }
