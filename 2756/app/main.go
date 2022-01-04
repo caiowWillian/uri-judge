3421ad04c1773ca6bce5192137df47a12fe5d422
Caio Willian <caiow.willian@gmail.com>
2022-01-03 23:16:14 -0300

    chore: resolve problem

---
+++ 2756/app/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	str := Output(0, "", false)
+	fmt.Println(str)
+}
+
+func Output(next int, strOutput string, invert bool) string {
+	words := [5]string{"A\n", "B B\n", "C   C\n", "D     D\n", "E       E\n"}
+	spaces := [5]string{"       ", "      ", "     ", "    ", "   "}
+
+	if next == -1 {
+		return strOutput
+	}
+
+	strOutput += spaces[next]
+	strOutput += words[next]
+
+	if !invert {
+		next++
+	} else {
+		next--
+	}
+
+	if next < 4 && !invert {
+		return Output(next, strOutput, false)
+	}
+	return Output(next, strOutput, true)
+}
